19c93d227349f9e088ae0b2eb897af96cd74b541
Wouter Heijligen <krabappel2548@gmail.com>
2013-06-13 08:32:37 +0200

    add release name and more dependencies

--- cm.mk
+++ cm.mk
@@ -1,3 +1,23 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2012 The Carbon Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# name
+PRODUCT_RELEASE_NAME := Huashan
+
 $(call inherit-product, device/sony/HuaShan/full_HuaShan.mk)
 
 # Inherit CM common GSM stuff.
@@ -12,6 +32,9 @@ TARGET_SCREEN_WIDTH := 720
 
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+#build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C5303_1273-0043/C5303:4.1.2/12.0.A.1.211/LPv_nw:user/release-keys PRIVATE_BUILD_DESC="6603-user 4.1.2 12.0.A.1.211 LPv_nw test-keys"
 
 PRODUCT_DEVICE := HuaShan
 PRODUCT_NAME := cm_HuaShan
