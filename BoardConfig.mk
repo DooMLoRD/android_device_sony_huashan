9da0b47633311d8f3055c4102c6271563d86aed6
Wouter Heijligen <krabappel2548@gmail.com>
2013-06-13 08:32:20 +0200

    Update boardconfig
    Add:
    - specific header path
    - kernel config
    - bluetooth path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,16 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 18
@@ -6,7 +19,6 @@ BOARD_VOLD_MAX_PARTITIONS := 18
 # since these partitions are not visible
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
-
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 BOARD_FLASH_BLOCK_SIZE := 131072
@@ -18,6 +30,7 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
+
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -25,7 +38,15 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_CUSTOM_GRAPHICS := ../../../device/sony/HuaShan/recovery/recovery.c
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/sony/HuaShan/recovery/recovery-keys.c
 
+# Assert
 TARGET_OTA_ASSERT_DEVICE := M36h,HuaShan
+
+TARGET_SPECIFIC_HEADER_PATH += device/sony/huashan/include
+
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/huashan/bluetooth
+
+TARGET_KERNEL_CONFIG := cm_viskan_huashan_defconfig
+
 
 # inherit from the proprietary version
 -include vendor/sony/HuaShan/BoardConfigVendor.mk
