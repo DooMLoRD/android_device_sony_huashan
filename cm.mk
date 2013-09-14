397cc45d90049b519555c346e08b3ea3258cb675
DooMLoRD <metoo.mememe@gmail.com>
2013-09-15 03:17:17 +0530

    fix: build fingerprint

--- cm.mk
+++ cm.mk
@@ -10,7 +10,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 #build.prop
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C5303_1273-0043/C5303:4.1.2/12.0.A.1.211/LPv_nw:user/release-keys PRIVATE_BUILD_DESC="6603-user 4.1.2 12.0.A.1.211 LPv_nw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303_1272-1874/C5303:4.1.2/12.0.A.2.254/Aff_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.1.2 12.0.A.2.254 Aff_nw test-keys"
 
 PRODUCT_NAME := cm_huashan
 PRODUCT_DEVICE := huashan
